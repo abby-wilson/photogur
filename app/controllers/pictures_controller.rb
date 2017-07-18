4d95bcdf57d5154db696d7bd7d6db673b5848244
Abby Wilson <abigail.wilson1@outlook.com>
2017-07-18 15:20:10 -0400

    Created edit and delete functions

--- app/controllers/pictures_controller.rb
+++ app/controllers/pictures_controller.rb
@@ -29,5 +29,28 @@ def new
     end
   end
 
+  def destroy
+    @picture = Picture.find(params[:id])
+    @picture.destroy
+    redirect_to "/pictures"
+  end
 
+  def edit
+    @picture = Picture.find(params[:id])
+  end
+
+  def update
+    @picture = Picture.find(params[:id])
+
+    @picture.title = params[:picture][:title]
+    @picture.artist = params[:picture][:artist]
+    @picture.url = params[:picture][:url]
+
+
+    if @picture.save
+      redirect_to "/pictures/#{@picture.id}"
+    else
+      render :edit
+    end
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -6,6 +6,11 @@ get 'pictures' => 'pictures#index'
 post 'pictures' => 'pictures#create'
 get 'pictures/new' => 'pictures#new'
 
+get 'pictures/:id/edit' => 'pictures#edit'
+patch 'pictures/:id' => 'pictures#update'
+
+delete 'pictures/:id' => 'pictures#destroy'
+
 get 'pictures/:id' => 'pictures#show'
 
 end
